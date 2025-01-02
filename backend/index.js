0f5ce75eb8c71fef18f4154ce014515109831fd3
squireel-bit <rahulytprem@gmail.com>
2025-01-02 13:18:06 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -17,10 +17,60 @@ app.use(bodyParser.json());
 
 
 
-const db = new pg.Client({
-  connectionString: process.env.POSTGRES_URL,
+const fs = require('fs');
+const url = require('url');
+
+const config = {
+    user: "avnadmin",
+    password: "AVNS_ZWk_tfodqvhC96EJlaa",
+    host: "pg-372176fb-rahulytprem-e006.g.aivencloud.com",
+    port: 12386,
+    database: "defaultdb",
+    ssl: {
+        rejectUnauthorized: true,
+        ca: `-----BEGIN CERTIFICATE-----
+MIIEQTCCAqmgAwIBAgIUCetStQALwBlY7qGiKUVzhEcNYu0wDQYJKoZIhvcNAQEM
+BQAwOjE4MDYGA1UEAwwvZWM0Mzc1MDgtNjZlZC00NGVlLWI2NzgtOThhNTI2NDk4
+YzlkIFByb2plY3QgQ0EwHhcNMjUwMTAyMDczNzAzWhcNMzQxMjMxMDczNzAzWjA6
+MTgwNgYDVQQDDC9lYzQzNzUwOC02NmVkLTQ0ZWUtYjY3OC05OGE1MjY0OThjOWQg
+UHJvamVjdCBDQTCCAaIwDQYJKoZIhvcNAQEBBQADggGPADCCAYoCggGBANWFQwMG
+iHkqpER1QUHFoZKOXPSpUiBLy4WweIjZ74ystd+E1+MGDhyuB3uiafhqNGYfKI8G
+ZM/vRPHkQKjiLhyd0Tuj4nWOgXevtPjrbHwxvrh19WH/9AxjULX8P9MFL3LfJeUU
+kZru75eAe+1wWUDQY4CpiETtzON8UQxkSCcG36OE9koPc9UjKVX0+C5WGtrQYKVP
+19x6Y6VIG29nNRsJcmjLa2//6gP/yFdJVY9zN3BJt2kdT74r6XsqHfuKwdJfO8Ts
+5TmxC8p6sPItpH/lADPqE0Xdnnfnp8St6XagQk+WVyu71JJnfCsVS+nJPXoSceM9
+fsTSfHtQ/9t6yQMO7DyicMiwkilw63SLAaqmoFXiA8YayIucEbXFQSqvi+3uhtxl
+oD33pVG23DNy6SL2YVgbDxQ+rZdviuFazF9B4AIYYtjLCr1FfMu9I+FlJunw1t6F
+Ewk7R+cICIe6CIxoa3VO+nIgCPkJ3D1gAU6jAIxzDI/Eh8iUjicZQO1JbQIDAQAB
+oz8wPTAdBgNVHQ4EFgQUCSMkLyg4YRZ0bb5wYVwhkWHVyfMwDwYDVR0TBAgwBgEB
+/wIBADALBgNVHQ8EBAMCAQYwDQYJKoZIhvcNAQEMBQADggGBACH8WFjluKbQ0Vlz
+WR6pvI2zCUZVQpJpZ6Rf22mg+CuOCRY44nKy6WOSsfL/xpA3Me9IM+cXUDac9JAJ
+ZVy3aIyP0N++thVAgQ8vHBefHNBaMcotJ+UuReqW0J3CyY+R/I6iliMTVdXjwzKT
+d7hRtCw74W2y2BCQNKp+9SX7jRFFEM2W+1bGIMA389s/1atMjiSLgIgu/1ASaw2i
+rZfEXLGMRG5Oob3qZdyP2/8FlmrlQzRuB6QmawguFujquo5VDRTwZA6k7SVA+eI4
+/txIGIgsCbQkos66BsfGRF78xFuvp28rqGYs1Hjb+GVNfSqnW92BMEDBQEwktmfZ
+XXtfbHnWHnlUgHM2q5Os1A53HX2Pgc3DZqt/cQk9ZfXaUK2JB4N8F/k/L/D64Vl3
+5z5uTLtKCOLoQrU2Dif9yCOyP1XfLXuNZXHk/fcNm7nDWqjKfjKV/52Gb7TIpBlg
+slw24NVwnVuBe/tVCoF7pjF5l8fOUHbDPo+Mq3/VwPHbCuUBHA==
+-----END CERTIFICATE-----`,
+    },
+};
+
+const client = new pg.Client(config);
+client.connect(function (err) {
+    if (err)
+        throw err;
+    client.query("SELECT VERSION()", [], function (err, result) {
+        if (err)
+            throw err;
+
+        console.log(result.rows[0].version);
+        client.end(function (err) {
+            if (err)
+                throw err;
+        });
+    });
 });
-db.connect();
 
 // Routes
 
