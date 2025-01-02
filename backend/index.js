0d6fb33a665c4777ac3439f1dcafdf15bf240bc1
Rahul <159545061+iamrahul-l@users.noreply.github.com>
2025-01-02 11:10:46 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -18,11 +18,7 @@ app.use(bodyParser.json());
 
 
 const db = new pg.Client({
-    user: process.env.POSTGRES_USER,
-    host: process.env.POSTGRES_HOST,
-    database: process.env.POSTGRES_DATABASE,
-    password: process.env.POSTGRES_PASSWORD,
-    port: process.env.POSTGRES_URL,
+  connectionString: process.env.POSTGRES_URL,
 });
 db.connect();
 
